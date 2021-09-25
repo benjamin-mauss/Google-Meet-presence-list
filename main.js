ebf95fec1dc491d200673179d02ae760108e8331
Benjamin Walter <waltermaussbenjamin@gmail.com>
2021-09-24 22:44:08 -0300

    created export functionality for json, txt and csv; still missing a preview and a better design

--- main.js
+++ main.js
@@ -8,10 +8,12 @@
 // I guess it does
 
 class user_data{
-    constructor(name, present, last_present_time){
+    constructor(name, present, last_time_present){
         this.name = name,
         this.present = present,
-        this.last_present_time = last_present_time
+        this.last_time_present = last_time_present,
+        this.spent_time = 0,
+        this.entry_time = last_time_present
     }
 }
 
@@ -32,11 +34,12 @@ class presence_list{
     }
 
     get_updated_users(){
-        return document.querySelectorAll('[jsmodel="cZWlhe QUjYIe"]')
+        return document.querySelectorAll('[jsmodel="cZWlhe QUjYIe"]')[0].firstElementChild.children[1].firstElementChild.firstElementChild
     }
 
     get_updated_usernames(updated_users){ // updated_users = get_updated_users()
         // popullating updated_names
+        console.log(updated_users)
         let updated_names = []
         for (let x=0;x<updated_users.length;x++){ 
             updated_names[x] =updated_users[x].textContent
@@ -55,7 +58,7 @@ class presence_list{
     }
 
     add_user(user){
-        return this.users.push(new user_data(user.name, user.present, user.last_present_time))
+        return this.users.push(new user_data(user.name, user.present, user.last_time_present))
     }
 
     // this is not intent to be used
@@ -70,8 +73,9 @@ class presence_list{
         return false
     }
 
-    update_users_states(){
+    update_users_states(automatic){
         
+        // it is called every X seconds and it updates the users array
         let updated_users = this.get_updated_users()
 
         if(!updated_users.length){
@@ -84,22 +88,22 @@ class presence_list{
                 let el2 = el[1].children[0].children[0]
                 
                 setTimeout(() => {
-                    console.log("Clicking in the button")
+                    // console.log("Clicking in the button")
                     el2.click()
                     setTimeout(() => {
-                        console.log("Clicking in the button again")
+                        // console.log("Clicking in the button again")
                         el2.click()
                     }, 500)
                 }, 500)
             }else{
-                console.log("Waiting for you enter in a meet")
+                // console.log("Waiting for you enter in a meet")
             }
             
             return
         }
         
-        if(updated_users.length == 1){ 
-            return console.log("Just you")
+        if(updated_users.length == 1 && !this.get_all_users().length){ 
+            return // console.log("Just you")
         }
 
         let current_time = this.get_time()
@@ -109,12 +113,12 @@ class presence_list{
         let updated_usernames = this.get_updated_usernames(updated_users)
 
         if(!all_usernames.length){ 
-            console.log("The first user entered")
+            // console.log("The first user entered")
             for(let n of updated_usernames){
                 this.add_user({
                     "name":n,
                     "present": true,
-                    "last_present_time": current_time
+                    "last_time_present": current_time
                 })
             }
             return
@@ -129,12 +133,12 @@ class presence_list{
                 this.add_user({
                     "name":updated_username,
                     "present": true,
-                    "last_present_time": current_time
+                    "last_time_present": current_time
                 })
             }else if(all_users[i]["present"] === false){ // if find it in the array, but the user was not present
                 console.log("A user has came back!" + updated_username)
                 all_users[i]["present"] = true
-                all_users[i]["last_present_time"] = current_time
+                all_users[i]["last_time_present"] = current_time
             }
         }
 
@@ -145,12 +149,16 @@ class presence_list{
             // if don't find the user in the updated array AND it is set has present
             if(updated_usernames.indexOf(all_users[x]["name"]) === -1 && all_users[x]["present"] === true){ 
                 // not present for the 1º update
-                console.log("A user has quit " + all_users[x]["name"])
+                // console.log("A user has quit " + all_users[x]["name"])
                 all_users[x]["present"] = false
             }
             // if find the user in the updated array AND it is set has present
             else if(all_users[x]["present"] === true){
-                all_users[x]["last_present_time"] = current_time
+                all_users[x]["last_time_present"] = current_time
+                // console.log(all_users[x])
+                if(automatic){
+                    all_users[x]["spent_time"] += time_between_updates/1000 // we want it to become seconds
+                }
             }
             // the user entered once, but he isn't here anymore
             else{
@@ -163,53 +171,271 @@ class presence_list{
         
     }
 
-    export_presence_list = class {
-        PDF(){
-            alert("generating pdf!")
-        }
-        CSV(){
-    
-        }
-        XLS(){
-    
-        }
-        JSON(){
-    
-        }
+    async get_storaged_file(filename){
+        let response = await fetch(chrome.runtime.getURL("resources/" + filename))
+        let r = await response.text()
+        return r
     }
+    
+    async createButton(){
 
+        // creating the button
 
-    createButton(){
         let insert_point =  document.querySelectorAll('[jsaction="rcuQ6b:npT2md;L3AHvb:FPkrPd;vqeE2:n3KREd;Gi6Dsf:xOekre;PRvZvb:wfXS6;C8MBwd:vaj4Sb"]')[0]
-        insert_point.insertAdjacentHTML('beforebegin',this.escapeHTMLPolicy.createHTML(`<div class="nod4rf PjGUeb" data-use-fullscreen="false" data-show-automatic-dialog="true"><div class="GKGgdd" data-is-muted="false" data-capture-type="sI3MNd" style=""><span data-is-tooltip-wrapper="true"><button class="super_id VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ tWDL4c uaILN bPEdgb qIiG8c FTMc0c N2RpBe jY9Dbb" data-disable-idom="true" aria-label="Presence List" data-tooltip-enabled="true" data-tooltip-id="tt-c2" data-tooltip-x-position="3" data-tooltip-y-position="2" role="button" data-is-muted="true" style="--mdc-ripple-fg-size:24px; --mdc-ripple-fg-scale:1.66667; --mdc-ripple-left:8px; --mdc-ripple-top:8px;"><div class="VfPpkd-Bz112c-Jh9lGc"></div><div class="SUtDBe a7Zbzb"><div aria-hidden="true" data-icon-type="4" class="IYwVEf uB7U9e"><div class="oTVIqe BcUQQ" aria-hidden="true"></div><span class="DPvwYc cR8Azd bkl1qf" aria-hidden="true">
-        <svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16ZM11.7069 6.70739C12.0975 6.31703 12.0978 5.68386 11.7074 5.29318C11.3171 4.9025 10.6839 4.90224 10.2932 5.29261L6.99765 8.58551L5.70767 7.29346C5.31746 6.90262 4.6843 6.90212 4.29346 7.29233C3.90262 7.68254 3.90212 8.3157 4.29233 8.70654L6.28912 10.7065C6.47655 10.8943 6.7309 10.9998 6.99619 11C7.26147 11.0002 7.51595 10.8949 7.70361 10.7074L11.7069 6.70739Z" fill="#ffffff"></path>
-        </svg>
-        </span><span class="DPvwYc zYrqtc thO1y" aria-hidden="true"><svg focusable="false" width="24" height="24" viewBox="0 0 24 24" class="Hdh4hc cIGbvc NMm5M"><path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z"></path><path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"></path></svg></span></div><div aria-hidden="true" data-icon-type="5" class="IYwVEf aC0Bke"><div class="oTVIqe BcUQQ" aria-hidden="true"></div><span class="DPvwYc cR8Azd bkl1qf" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="#000000" class="Hdh4hc cIGbvc"><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span><span class="DPvwYc zYrqtc thO1y" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="#000000" class="Hdh4hc cIGbvc"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span></div></div></button><div class="EY8ABd-OWXEXe-TAWMXe" role="tooltip" aria-hidden="true" id="tt-c2">Presence List</div></span></div></div>
-        `))
-
-        let inserted_button =  document.getElementsByClassName('super_id')[0]
-        console.log()
         
-        inserted_button.addEventListener("click", (e) => {
-            e.preventDefault()
-            alert("Here you go, Pablo");
-            // export_presence.PDF()
+        let button_content = await this.get_storaged_file("button.html")
+        let popup_content = await this.get_storaged_file("popup.html")
+        let style_content = await this.get_storaged_file("style.css")
+                
+        insert_point.insertAdjacentHTML('beforebegin',this.escapeHTMLPolicy.createHTML(`
+        <!-- Start of Google Meet Presence List extension -->
+            <!-- Start of Button -->
+            ${button_content}
+            <!-- End of Button -->
+
+            <!-- Start of Popup-->
+            ${popup_content}
+            <!-- End of Popup-->
+
+            <!-- Start of Style-->
+            <style>
+            ${style_content}
+            </style>
+            <!-- End of Style -->
+        <!-- End of Google Meet Presence List extension -->
+        `))
+        
+        
+        let inserted_button =  document.getElementsByClassName('super_id')[0]
+        
+        // creating the popup js code
+        const openModalButtons = document.querySelectorAll('[data-modal-target]')
+        const closeModalButtons = document.querySelectorAll('[data-close-button]')
+        const overlay = document.getElementById('overlay')
+
+        openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.modalTarget)
+            openModal(modal)
+        })
         })
 
+        overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active')
+        modals.forEach(modal => {
+            closeModal(modal)
+        })
+        })
+
+        closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal')
+            closeModal(modal)
+        })
+        })
+
+        function openModal(modal) {
+        if (modal == null) return
+        modal.classList.add('active')
+        overlay.classList.add('active')
+        }
+
+        function closeModal(modal) {
+        if (modal == null) return
+        modal.classList.remove('active')
+        overlay.classList.remove('active')
+        }
+
+        document.getElementById("exportToTEXT").addEventListener('click', () => {
+            export_presence.TEXT()
+        })
+        // document.getElementById("exportToXLS").addEventListener('click', () => {
+        //     export_presence.XLS()
+        // })
+        document.getElementById("exportToJSON").addEventListener('click', () => {
+            export_presence.JSON()
+        })
+        document.getElementById("exportToCSV").addEventListener('click', () => {
+            export_presence.CSV()
+        })
+
+
+
+
+
     }
         
 }
 
+class export_presence_list {
+    TEXT(){
+        google_meet_presence_list.update_users_states()
+        let user_list = google_meet_presence_list.get_all_users()
+        // console.log(user_list)
+        if(!this.check_users(user_list)){
+            return
+        }
 
+        let lines = ""
+        
+        if(document.getElementById("name").checked){
+            lines += "name,"
+        }
+        if(document.getElementById("entry_time").checked){
+            lines += "entry_time,"
+        }
+        if(document.getElementById("last_time").checked){
+            lines += "last_time,"
+        }
+        if(document.getElementById("spent_time").checked){
+            lines += "spent_time,"
+        }
+        
+        lines = lines.substr(0,lines.length-1) // removes the comma in the end
+        for(let i = 0; i < user_list.length; i++){
+            let line = "\n"
+                       
+            if(document.getElementById("name").checked){
+                line += user_list[i]["name"] + ","
+            }
+            if(document.getElementById("entry_time").checked){
+                line += user_list[i]["entry_time"] + ","
+            }
+            if(document.getElementById("last_time").checked){
+                line += user_list[i]["last_time_present"] + ","
+            }
+            if(document.getElementById("spent_time").checked){
+                line += user_list[i]["spent_time"] + ","
+            }
+            line = line.substr(0,line.length-1) // removes the comma in the end
+            lines += line
+        }
+        this.downloadFile(lines, "presence_list.txt", "text/plain") 
+
+    }
+   
+    CSV(){
+        google_meet_presence_list.update_users_states()
+        let user_list = google_meet_presence_list.get_all_users()
+        // console.log(user_list)
+        if(!this.check_users(user_list)){
+            return
+        }
+
+        let lines = ""
+        
+        if(document.getElementById("name").checked){
+            lines += "name,"
+        }
+        if(document.getElementById("entry_time").checked){
+            lines += "entry_time,"
+        }
+        if(document.getElementById("last_time").checked){
+            lines += "last_time,"
+        }
+        if(document.getElementById("spent_time").checked){
+            lines += "spent_time,"
+        }
+        
+        lines = lines.substr(0,lines.length-1) // removes the comma in the end
+        for(let i = 0; i < user_list.length; i++){
+            let line = "\n"
+                       
+            if(document.getElementById("name").checked){
+                line += user_list[i]["name"] + ","
+            }
+            if(document.getElementById("entry_time").checked){
+                line += user_list[i]["entry_time"] + ","
+            }
+            if(document.getElementById("last_time").checked){
+                line += user_list[i]["last_time_present"] + ","
+            }
+            if(document.getElementById("spent_time").checked){
+                line += user_list[i]["spent_time"] + ","
+            }
+            lines += line.substr(0,line.length-1) // removes the comma in the end
+        }
+        this.downloadFile(lines, "presence_list.csv", "text/csv") 
+
+    }
+    // XLS(){
+
+    // }
+    JSON(){
+        google_meet_presence_list.update_users_states()
+        let user_list = google_meet_presence_list.get_all_users()
+        if(!this.check_users(user_list)){
+            return
+        }
+        let user_list_output = []
+        for(let u=0;u < user_list.length; u++){
+            let tmp = {}
+            if(document.getElementById("name").checked){
+                tmp["name"] = user_list[u]["name"]
+            }
+            if(document.getElementById("entry_time").checked){
+                tmp["entry_time"] = user_list[u]["entry_time"]
+            }
+            if(document.getElementById("last_time").checked){
+                tmp["last_time"] = user_list[u]["last_time_present"]
+            }
+            if(document.getElementById("spent_time").checked){
+                tmp["spent_time"] = user_list[u]["spent_time"]
+            }
+            
+            // console.log(tmp)
+            user_list_output.push(tmp)
+        }
+        
+        this.downloadFile(JSON.stringify(user_list_output, null, 2), "presence_list.json", "application/json")
+    }
+    
+    check_users(user_list){
+        
+        if(!user_list.length){
+            // console.log("No one besider you")
+            let m = document.getElementById("message")
+            m.innerText = "There is no one, besides yourself, in this room"
+            setTimeout((m) => {
+                // console.log(m)
+                m.innerText = ""
+            }, 3000,m)
+            return false
+        }else{
+            // console.log("aee")
+            return true
+        }
+        
+    }
+
+    downloadFile(data, filename, type) { 
+        
+        // https://stackoverflow.com/questions/13405129/javascript-create-and-save-file#answer-30832210
+        var file = new Blob([data], {type: type});
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);  
+            }, 0); 
+        }
+    }
+}
 
 var google_meet_presence_list = new presence_list
-var export_presence = new google_meet_presence_list.export_presence_list
+var export_presence = new export_presence_list
+google_meet_presence_list.update_users_states()
 
-var ext_export_presence_list = () => {
-    alert(123)
-    export_presence.PDF()
-}
+var time_between_updates = 5000 // 5 seconds
+
 setInterval(() => {
-    google_meet_presence_list.update_users_states()
-}, 5000) // each 5 seconds, it calls update()
+    google_meet_presence_list.update_users_states(true)
+}, time_between_updates) // each 5 seconds, it calls update()
+
